a02f2d9c84de99cc464980ee1a45545115600543
IMSHoJung <IMSHoJung@imsmobility.co.kr>
2023-12-03 16:48:46 +0900

    feat: user dto description

--- src/user/user.dto.ts
+++ src/user/user.dto.ts
@@ -3,24 +3,28 @@ import { IsEmail, IsNotEmpty } from 'class-validator';
 
 // 유저 생성 DTO
 export class CreateUserDto {
-  @ApiProperty()
+  @ApiProperty({ description: '이메일' })
   @IsEmail()
+  @IsNotEmpty()
   email: string;
 
-  @ApiProperty()
+  @ApiProperty({ description: '이름' })
   @IsNotEmpty()
   name: string;
 
-  @ApiProperty()
+  @ApiProperty({ description: '패스워드' })
   @IsNotEmpty()
   password: string;
 }
 
 // 로그인 DTO
 export class UserLoginDto {
-  @ApiProperty()
+  @ApiProperty({ description: '이메일' })
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
 
-  @ApiProperty()
+  @ApiProperty({ description: '패스워드' })
+  @IsNotEmpty()
   password: string;
 }
